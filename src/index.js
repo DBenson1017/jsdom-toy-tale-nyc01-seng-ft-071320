dd9f5aab976e5d36b045da4b8db2fe58d6cabce3
DBenson1017 <devinbenson@gmail.com>
2020-09-01 22:57:28 -0400

    need to continue passing data from create new form into configObj

--- src/index.js
+++ src/index.js
@@ -13,104 +13,94 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  // May need to do a patch fetch to change the lines in the json .db
-  // function addLike(e){
-  //     e.preventDefault()
-  //     let crd= e.target.parentNode
-  //     console.log('setting card to variable worked')
-  //     lks= crd.querySelector('p')
-  //     console.log('finding p worked')
-  //     lks.value = parseInt(lks.value, 10)
-  //     // lks.value++
-  //     console.log('likes should have incremented')
-  // }
+  let addToyBtn = document.querySelector('.add-toy-form')
+  let toyCollection = document.querySelector('#toy-collection')
 
-  function renderToys(object) {
-   //a function to receieve toy instances from the iterator (from the 'getToys()' function) in the fetch response, which appends it onto the DOm 
-    let card = document.createElement('div')
-    card.setAttribute('class', 'cards')
-    let h2 = document.createElement('h2')
-    h2.innerText = object.name
-    card.append(h2)
-    let pic = document.createElement('img')
-    pic.src = object.image
-    card.append(pic)
-    let likes = document.createElement('p')
-    likes.innerText = object.likes
-    let liker = document.createElement('button')
-    liker.setAttribute('class', 'like-btn')
-    liker.setAttribute('id', object.id)
-    liker.innerText = "like"
-    card.append(likes)
-    card.append(liker)
-    liker.addEventListener('click', function(e) {
-      addLike(e)
-      // console.log(e.target.parentNode)
-      // console.log(e.target.parentNode)
-      // console.log(e.target.parentNode)
-      // // likes(e)
+  const toyFetch = () =>
+  fetch('http://localhost:3000/toys')
+    .then(function (response){
+      return response.json()
     })
-    let collection = document.querySelector('#toy-collection')
-    collection.append(card)
+    .then(function (data){
+      toyIterator(data)
+      console.log(data)
+    })
+  // end of toyFetch 
+
+  const toyIterator= data => {
+    console.log('entered toyIterator')
+    data.forEach(toy => renderToys(toy))
+  } // end of toyRender 
+
+  const renderToys= (toy) => {
+    console.log('enter renderToys')
+    // console.log(toy.name)
+    // console.log(toy)
+    let card = document.createElement('div')
+    console.log(card)
+    card.setAttribute('class', 'card')
+    card.innerHTML=` 
+          <h2> ${toy.name}</h2>
+          <img src= ${toy.image} class='toy-avatar' /> 
+          <p>${toy.likes}Likes</p> 
+          <button class='like-btn'>Like <3 </button>   
+    `
+    toyCollection.append(card)
+  } // end of renderToys
+
+
+// #####create new toy card #################################
+
+//event listener for create button click 
+//prevent form default
+//grab input values 
+//pass to function for onject 
+//pass to fetch request 
+
+document.addEventListener('submit', function(e){
+  e.preventDefault()
+  console.log('new toy clicked')
+      if (e.target === addToyBtn){
+        console.log(e.target.closest('.add-toy-form'))
+        let name = e.target.name
+        let image = e.target.image
+        let data = {
+          name: name,
+          image: image, 
+          likes: 0
+        }
+        console.log(data)
+        // createNew(data)
+      
+    }
+})// end of crete new to listener
+
+function createNew(data){
+  let options={
+    method: "POST",
+    headers: {
+      'content-type':"application/json",
+      'accept':'application/'
+    },
+    body: JSON.stringify(data)
   }
 
-  //button event listener that ++ likes 
-
-  // fetch request to the toys json api 
-  function getToys(){
-    fetch("http://localhost:3000/toys").then(function(response){
+  fetch('http://localhost:3000/toys', options)
+    .then(function (response){
       return response.json();
     })
-      .then(function(object){
-        // iterates through the json response, each instance is sent to the renderToys function above 
-        object.forEach(object => renderToys(object)) 
-     })
-  }
-  getToys() //automatically runs the fetch() once DOM is loaded 
+    .then (function (object){
+      console.log(object)
+      renderToys(object)
+    })
 
-  // function postToys(){
-      // Post the data to the http addresss
-      // collect the data the from the form, including a preventDefault
-      
-      // define our configObj ; HHTP verb, headers/metadata, data stringified in body 
-
-     
+} // end of createNew
 
 
-      //POST fetch request with configObj
 
-      //conver the returned data to a usable object 
-      //pass that object through the renderToys function 
 
-  function postToy() {
-    
-      let nameInput = document.getElementsByClassName('input-text').name.innerText
-      let imgInput = document.getElementsByClassName('input-text').image.innerText
 
-     let data = {
-            "name": nameInput, 
-            "image": imgInput,
-            "likes": 0
-     }
 
-     let configObj = {  
-      method: 'POST',
-      headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-           },
-      body: JSON.stringify(data)
-      }
-    
-    fetch('http://localhost:3000/toys', configObj)
-          .then(function(response){
-            return response.json();
-          })
-          .then(function(object){
-            renderToys(object);
-          })
-          }
-    
-  
+toyFetch()
 
 }); // end of DOM Content Loader listener 
